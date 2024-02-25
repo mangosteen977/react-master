9f9ad7a7c7a36da27b1b3efb8312b46d17fc71ea
mangosteen977 <mosubin977@daum.net>
2024-02-25 17:46:16 +0900

    set html attr in the style-components by [.attrs({html tag attribute})], replace the html tag in the style-components by [as="html tag name"]

--- styled-components/src/App.js
+++ styled-components/src/App.js
@@ -3,23 +3,19 @@ import styled from "styled-components";
 const Father = styled.div`
  display : flex;
 `;
-//back tick (`)으로 사이에 css코드를 적어서 스타일 컴포넌츠 생성
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color : ${(props) => props.bgColor};
+
+const Input = styled.input.attrs({ required : true, minLength : 5 })`
+  background-color : tomato
 `;
-//styled-components에서 'bgColor'로 props를 보내서 css 속성 적용
-const Circle = styled(Box)`
-  border-radius : 50px;
-`;
-// styled-components 확장 (기존 + 추가 속성)
+// 잘 사용하는 방법은 아니지만, style-components에서 attrs.({tag 속성})지정 가능
+
 
 function App() {
   return (
-    <Father>
-      <Circle bgColor="tomato" />
-      <Box bgColor="teal" />
+    <Father as="header">
+      {/* as : style-components의 속성은 유지하되, html tag 바꾸기 */}
+      <Input />
+      <Input />
     </Father>
   );
 }
