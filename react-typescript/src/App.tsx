5f2f132e9ebb6fe1079d3d94c62ebb8df466308a
mangosteen977 <mosubin977@daum.net>
2024-03-04 17:16:22 +0900

    set type of event [(event: React.FormEvent<HTMLFormElement>)]

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,10 +1,41 @@
 import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // event: React.FormEvent<HTMLInputElement> : event의 type지정
+    // const {value} = event.currentTarget;
+    const {
+      currentTarget: { value },
+    } = event;
+    // 예)  event = { x, y }일 때, const { x, y } = event;이면, x는 event.x, y는 event.y
+    // event = { currentTarget : { value : "", ...}, ... }
+    // const { currentTarget : { value : "", ...}, ... } = event;
+    // const value = event.currentTarget.value;
+    /*
+      ES6 문법
+      > const value = event.currentTarget.value
+      > const {value} = event.currentTarget
+      > const {currentTarget:{value}} = event
+    */
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
   return (
     <div>
-      <Circle bgColor="tomato" />
-      <Circle bgColor="pink" borderColor="hotpink" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Login</button>
+      </form>
     </div>
   );
 }
