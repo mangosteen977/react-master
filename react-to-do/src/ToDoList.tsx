12eae77250a7b2d76173e693bf04ee870b3f8e80
mangosteen977 <mosubin977@daum.net>
2024-03-19 01:40:40 +0900

    [react-to-do] RegisterOptions의 pattern으로 입력값 정규식으로 확인, errors.message 보이도록 수정, useForm의 type 명시(interface)

--- react-to-do/src/ToDoList.tsx
+++ react-to-do/src/ToDoList.tsx
@@ -35,15 +35,35 @@ function ToDoList() {
   );
 }
 */
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  password: string;
+  password1: string;
+}
 function ToDoList() {
-  const { register, watch, handleSubmit, formState } = useForm();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      // 프로퍼티 기본 값 설정
+      email: "@naver.com",
+    },
+  });
   // register() : onChange, onBlur(입력 커서 focus가 input에 있지 않은 상태)이벤트 반환
   // watch() : form의 입력값 변화를 관찰
   const onValid = (data: any) => {
     console.log(data);
   };
   // handleSubmit() : onSubmit 대체, validation, preventDefault
-  console.log(formState.errors);
+  // handleSubmit은 onValid()함수 필요
+  // handleSubmit(onValid(required), oninValid(optional))
+  console.log(errors);
   // formState : formState.errors에서 required, minLength 등 input의 error 확인
   // password1:{type: 'minLength', message: '', ref: input}
   return (
@@ -52,40 +72,71 @@ function ToDoList() {
         style={{ display: "flex", flexDirection: "column" }}
         onSubmit={handleSubmit(onValid)}
       >
-        <input {...register("Email", { required: true })} placeholder="Email" />
+        <input
+          {...register("email", {
+            required: "email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              // 정규식으로 패턴 확인도 가능
+              // ^ :문장의 시작
+              // [] : 문자셋 안의 아무문자
+              // + : 하나 또는 많이
+              message: "Only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
         {/* HTML코드 required 등 대신 react-hook-form의 RegisterOptions으로 { required: true }등 사용. 
             RegisterOptions이 입력되지 않았을 경우, form입력값이 onValid로 넘어가지 않음,
             입력되지 않은 값의 input에 focus동작.
+            
         */}
+        <span style={{ color: "red" }}>{errors.email?.message}</span>
         <input
-          {...register("FirstName", {
+          {...register("firstName", {
             required: "First name is required",
             minLength: {
               value: 2,
               message: "your first name is way too short",
             },
           })}
-          required
-          placeholder="FirstName"
+          placeholder="firstName"
         />
         {/* RegisterOptions에 대해 메세지 설정 가능,
         각종 조건에 대한 내용은 Ctrl + required클릭 (validator.d.ts) */}
+        <span style={{ color: "red" }}>{errors.firstName?.message}</span>
         <input
-          {...register("LastName", { required: true })}
-          placeholder="LastName"
+          {...register("lastName", { required: "Write here" })}
+          placeholder="lastName"
         />
+        <span style={{ color: "red" }}>{errors.lastName?.message}</span>
         <input
-          {...register("UserName", { required: true, minLength: 10 })}
-          placeholder="UserName"
+          {...register("userName", {
+            required: "Write here",
+            minLength: {
+              value: 2,
+              message: "your first name is way too short",
+            },
+          })}
+          placeholder="userName"
         />
+        <span style={{ color: "red" }}>{errors.userName?.message}</span>
         <input
-          {...register("password", { required: true, minLength: 5 })}
+          {...register("password", { required: "Write here" })}
           placeholder="password"
         />
+        <span style={{ color: "red" }}>{errors.password?.message}</span>
         <input
-          {...register("password1", { required: true, minLength: 10 })}
+          {...register("password1", {
+            required: "Write here",
+            minLength: {
+              value: 2,
+              message: "your first name is way too short",
+            },
+          })}
           placeholder="password1"
         />
+        <span style={{ color: "red" }}>{errors.password1?.message}</span>
         {/* {...register("toDo")} 
             register()가 반환하는 객체들을 input의 props로 펼쳐서 줌.
             "Email"는 name
