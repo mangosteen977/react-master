fa7e9b58a5ab300d88278fafd88dfdb34b48bc12
mangosteen977 <mosubin977@daum.net>
2024-03-19 00:17:43 +0900

    [react-to-do] react-hook-from (form 라이브러리)의 useForm을 사용해서 form의 입력값 변화를 관찰 [register, watch]

--- react-to-do/src/ToDoList.tsx
+++ react-to-do/src/ToDoList.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
+/* 기존의 useState를 이용해 form+input 사용
 function ToDoList() {
   const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
     setToDo(value);
+    setToDoError("");
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(toDo);
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    console.log("submit");
   };
   return (
     <div>
@@ -21,6 +28,31 @@ function ToDoList() {
           type="text"
           placeholder="Write a to do"
         />
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
+    </div>
+  );
+}
+*/
+function ToDoList() {
+  const { register, watch } = useForm();
+  // register() : onChange, onBlur(입력 커서 focus가 input에 있지 않은 상태)이벤트 반환
+  // watch() : form의 입력값 변화를 관찰
+  console.log(register("toDo"));
+  return (
+    <div>
+      <form>
+        <input {...register("Email")} placeholder="Email" />
+        <input {...register("FirstName")} placeholder="FirstName" />
+        <input {...register("LastName")} placeholder="LastName" />
+        <input {...register("UserName")} placeholder="UserName" />
+        <input {...register("password")} placeholder="password" />
+        <input {...register("password1")} placeholder="password1" />
+        {/* {...register("toDo")} 
+            register()가 반환하는 객체들을 input의 props로 펼쳐서 줌.
+            "Email"는 name
+        */}
         <button>Add</button>
       </form>
     </div>
