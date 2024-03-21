c28193f3b99bbd2f45ac5419cc9da2b8bb7e59aa
mangosteen977 <mosubin977@daum.net>
2024-03-21 14:17:42 +0900

    [Recoil - Selector] Recoil Selector의 get(), set() 함수 사용해 Minute <=> Hour 변환 (set의 SetterOrUpdater()로 newValue 받기, set()으로 atomState의 값 설정하기)

--- kanban-board/src/App.tsx
+++ kanban-board/src/App.tsx
@@ -1,5 +1,37 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hoursSelectorAtom, minutesStateAtom } from "./atom";
+
 function App() {
-  return <div></div>;
+  const [minutes, setMinutes] = useRecoilState(minutesStateAtom);
+  const [hours, setHours] = useRecoilState(hoursSelectorAtom);
+  // [get()값, set()값]
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+    // string숫자 앞에 + => number
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+    // string숫자 앞에 + => number
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <span>분</span>
+      <div>↔</div>
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+      />
+      <span>시간</span>
+    </div>
+  );
 }
 
 export default App;
