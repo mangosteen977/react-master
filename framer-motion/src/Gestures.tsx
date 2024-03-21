bdcc7a496cf663ebda67d532cc7008d71f4eeffc
mangosteen977 <mosubin977@daum.net>
2024-03-22 01:01:05 +0900

    [framer-motion] drag 범위 제한 (dragConstraints, 부모객체 ref), 드래그 이후 제자리/탄력성 (dragSnapToOrigin, dragElastic)

--- framer-motion/src/Gestures.tsx
+++ framer-motion/src/Gestures.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -19,6 +20,16 @@ const Box = styled(motion.div)`
   border-radius: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  overflow: hidden;
+`;
 const boxVariants = {
   hover: { scale: 1.5, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
@@ -26,14 +37,26 @@ const boxVariants = {
 };
 
 function Gestures() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
-    <Box
-      drag // drag 붙이면 드래그에 제한이 없는 드래깅
-      whileDrag="drag" // whileDrag로 드래그 하는 동안의 동작 정의
-      variants={boxVariants}
-      whileHover="hover"
-      whileTap="click"
-    />
+    <BiggerBox ref={biggerBoxRef}>
+      <Box
+        drag
+        dragConstraints={biggerBoxRef}
+        /* drag 이동 범위
+        아무 속성 없으면 제한 없음
+        drag ="x/y : 해당 축으로만 드래그가 가능하게 제한
+        dragConstraints={{ top: -200, bottom: 200, left: -200, right: 200 }} : top/bottom/left/right 픽셀로 제한
+        dragConstraints={biggerBoxRef} : 부모객체 ref, 부모객체 안으로 제한
+        */
+        dragSnapToOrigin // 드래그 이후 원자리로 돌아옴
+        dragElastic={0.5} // 드래그에 탄력을 줌 (0~1)
+        whileDrag="drag" // whileDrag로 드래그 하는 동안의 동작 정의
+        variants={boxVariants}
+        whileHover="hover"
+        whileTap="click"
+      />
+    </BiggerBox>
   );
 }
 export default Gestures;
