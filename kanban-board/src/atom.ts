7457913606741637f10ca1b9eba5163278a1317f
mangosteen977 <mosubin977@daum.net>
2024-04-04 13:37:00 +0900

    [kanban-board] 로컬스토리지 저장을 위해 recoil-persist install 및 적용

--- kanban-board/src/atom.ts
+++ kanban-board/src/atom.ts
@@ -1,4 +1,10 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  key: "trelloBoardLocal",
+  storage: localStorage,
+});
 
 export interface IToDo {
   id: number;
@@ -10,23 +16,16 @@ interface IToDoStateAtom {
 export const toDoStateAtom = atom<IToDoStateAtom>({
   key: "toDo",
   default: {
-    "To Do": [
-      { id: 1, text: "todo1" },
-      { id: 2, text: "todo2" },
-    ],
-    Doing: [{ id: 3, text: "doing" }],
-    Done: [
-      { id: 4, text: "done1" },
-      {
-        id: 5,
-        text: "오늘은 밥을 먹고 양치를 하고 커피를 마셨다. 아뿔싸 또 다시 양치를 해야하는구나!",
-      },
-    ],
+    "To Do": [],
+    Doing: [],
+    Done: [],
   },
+  effects_UNSTABLE: [persistAtom],
 });
 
 //Dark/Light Mode
 export const ToggleThemeAtom = atom<boolean>({
   key: "toggleTheme",
   default: true,
+  effects_UNSTABLE: [persistAtom],
 });
